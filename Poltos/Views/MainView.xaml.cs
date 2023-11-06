19073ba9e4c97f19ec564be9270cb259407a821b
Ivan <vaneklopatin.main@gmail.com>
2023-11-07 00:01:09 +0300

    Добавьте файлы проекта.

---
+++ Poltos/Views/MainView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Interop;
+
+namespace Poltos.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+         
+        }
+
+        /// <summary>
+        /// Event Handlers
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
+    }
+}
